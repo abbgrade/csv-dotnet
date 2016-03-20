3bd4c19b4f0d0d9f027228559c5831994bd2cd21
Steffen Kampmann <steffen.kampmann@gmail.com>
2016-03-20 14:56:37 +0100

    add StreamReader extension example

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("CSV")]
+[assembly: AssemblyDescription("A simple .NET Library for CSV-File access.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("csv-dotnet")]
+[assembly: AssemblyCopyright("MIT License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("2dfc85a3-9835-4a43-9d54-34a843a620bb")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # csv-dotnet
 A simple .NET Library for CSV-File access.
+
+# Usage
+
+## StreamReader Extension Example
+
+Import the CSV library to extend the StreamReader Class.
+
+	use CSV;
+
+Use the StreamReader.ReadRows enumerator to get the rows.
+
+	using (var reader = new StreamReader("logfile.csv", Encoding.Default))
+	{
+		foreach (var line in reader.ReadRows('\n', ',', 1))
+		{
+			Console.WriteLine(string.Join("\t", line));
+		}
+	}
